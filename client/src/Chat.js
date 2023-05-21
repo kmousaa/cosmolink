ba8920f81aaa074e7d8684aa627314e549b4ff03
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2023-05-22 00:45:20 +0100

    Added functionality to atatch file button

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -9,23 +9,47 @@ function Chat({ socket, username, room }) {
   const messageContainerRef = useRef(null);
 
   const sendMessage = () => {
-    if (currentMessage !== '') {
+    if (file) {
       const messageData = {
         room: room,
         author: username,
+        type: 'file',
+        mimeType: file.type,
+        fileName: file.name,
         message: currentMessage,
-        time: new Date(Date.now()).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+        time: new Date(Date.now()).toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit'
+        })
       };
-      socket.emit('send_message', messageData);
-      setMessageList((list) => [...list, messageData]);
       setCurrentMessage('');
+      setFile();
+      setFileSelected(false);
+      socket.emit('send_message', messageData);
+    } else {
+      if (currentMessage !== '') {
+        const messageData = {
+          room: room,
+          author: username,
+          type: 'text',
+          message: currentMessage,
+          time: new Date(Date.now()).toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit'
+          })
+        };
+        socket.emit('send_message', messageData);
+        setMessageList((list) => [...list, messageData]);
+        setCurrentMessage('');
+      }
     }
   };
 
   const selectFile = (e) => {
-    // Handle file selection logic here
-    setCurrentMessage(e.target.files[0].name);
-    setFile(e.target.files[0]);
+    const selectedFile = e.target.files[0];
+    setCurrentMessage(selectedFile.name);
+    setFile(selectedFile);
+    setFileSelected(true);
   };
 
   const handleKeydown = (e) => {
@@ -48,32 +72,57 @@ function Chat({ socket, username, room }) {
     return () => {
       socket.off('receive_message', receiveMessage);
     };
-  }, [socket, messageList]);
+  }, [socket]);
+
+  const renderMessage = (message, index) => {
+    if (message.type === 'file') {
+      return (
+        <div
+          className={`flex flex-col py-1 ${
+            message.author === username ? 'self-end items-end' : 'items-start'
+          }`}
+          key={index}
+        >
+          <p className="font-bold">{message.author}</p>
+          <img
+            style={{ width: 150, height: 'auto' }}
+            src={message.blob}
+            alt={message.fileName}
+          />
+          <span className="text-xs text-gray-100">{message.time}</span>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className={`flex flex-col py-1 ${
+            message.author === username ? 'self-end items-end' : 'items-start'
+          }`}
+          key={index}
+        >
+          <p className="font-bold">{message.author}</p>
+          <p className="bg-blue-100 rounded-lg px-3 py-2">{message.message}</p>
+          <span className="text-xs text-gray-100">{message.time}</span>
+        </div>
+      );
+    }
+  };
 
   return (
     <div className="flex flex-col h-screen mx-10 my-3">
       <div className="flex-1 overflow-y-auto px-4 py-2" ref={messageContainerRef}>
-        {messageList.map((messageContent, index) => (
-          <div
-            className={`flex flex-col py-1 ${messageContent.author === username ? 'self-end items-end' : 'items-start'}`}
-            key={index}
-          >
-            <p className="font-bold">{messageContent.author}</p>
-            <p className="bg-blue-100 rounded-lg px-3 py-2">{messageContent.message}</p>
-            <span className="text-xs text-gray-100">{messageContent.time}</span>
-          </div>
-        ))}
+        {messageList.map((messageContent, index) => renderMessage(messageContent, index))}
       </div>
       <div className="p-4 bg-gray-100 grid-cols-3 rounded-full">
         <div className="flex items-center">
-          
-          {/* add file */}
-          <label className={`mr-3 px-4 py-2 ${fileSelected == false ? `bg-gray-300` : `bg-green-300`}  text-white rounded-full focus:outline-none`}>
-            <div className='font-bold'> {fileSelected == false ? '+' : '✓'} </div>
+          <label
+            className={`mr-3 px-4 py-2 ${
+              fileSelected === false ? 'bg-gray-300' : 'bg-green-300'
+            } text-white rounded-full focus:outline-none`}
+          >
+            <div className="font-bold">{fileSelected === false ? '+' : '✓'}</div>
             <input onChange={selectFile} type="file" className="hidden" />
           </label>
-
-          {/* textbox */}
           <input
             type="text"
             className="flex-1 rounded-full py-2 px-5 bg-white focus:outline-none"
@@ -82,8 +131,6 @@ function Chat({ socket, username, room }) {
             onChange={(event) => setCurrentMessage(event.target.value)}
             onKeyDown={handleKeydown}
           />
-
-          {/* send button */}
           <button
             className="ml-4 px-4 py-2 bg-blue-500 text-white rounded-full focus:outline-none"
             onClick={sendMessage}
