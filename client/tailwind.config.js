d4c4e6f1b26eccacb8511de73d8018c3204751df
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2023-05-21 14:50:04 +0100

    Created GUI for landing page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      "ibm-plex-mono": "var(--ibm-plex-mono)",
+      "ibm-plex-mono-light": "var(--ibm-plex-mono-light)",
+      quicksand: "var(--quicksand)",
+      "quicksand-light": "var(--quicksand-light)",
+      "quicksand-bold": "var(--quicksand-bold)",
+      "major-mono-display": "var(--major-mono-display)",
+    },
     extend: {},
   },
   plugins: [],
